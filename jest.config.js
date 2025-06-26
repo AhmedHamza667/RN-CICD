f5f2d6fac0cff37aac1f4fbd3887bc0d24c4eeb9
AhmedHamza667 <hamzaa667788@gmail.com>
2025-06-26 12:43:21 -0400

    yaml file

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    "node_modules/(?!(react-native|@react-native|@react-navigation|react-clone-referenced-element|@react-native-community|@react-native-picker|@react-native-async-storage|@react-native-masked-view|@react-native-segmented-control|@react-native-firebase|@react-native-google-signin|@react-native-safe-area-view|@react-native-safe-area-context|@react-native-vector-icons|@react-native-community/async-storage|@react-native-community/netinfo|@react-native-community/viewpager|@react-native-community/slider|@react-native-community/cameraroll|@react-native-community/clipboard|@react-native-community/datetimepicker|@react-native-community/geolocation|@react-native-community/hooks|@react-native-community/masked-view|@react-native-community/picker|@react-native-community/push-notification-ios|@react-native-community/toolbar-android|@react-native-community/viewpager|@react-native-community/webview)/)"
+  ],
 };
